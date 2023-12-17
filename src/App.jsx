3cb8501f4e5b391f46b60f6d8cd2cac30c8ee9b4
pabcrudel <pabcrudel@gmail.com>
2023-12-17 21:07:24 +0100

    add: Get and print a cat fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+
 export default function App () {
+  const [fact, setFact] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(res => setFact(res.fact));
+  }, []);
+
   return (
     <>
-      <h1>Hello world</h1>
+      <header>
+        <h1>Hello world</h1>
+      </header>
+
+      <main>
+        {fact && <p>{fact}</p>}
+      </main>
     </>
   );
 }
