f44b9ab09b8f060854469d5f688d46e724125d57
pabcrudel <pabcrudel@gmail.com>
2023-12-17 21:49:48 +0100

    add: Cat image with the first three words of the fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react';
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const catEndpointRandomPhoto = word => 'https://cataas.com/cat/says/' + word +
+  '?fontSize=50&fontColor=red';
 
 export default function App () {
   const [fact, setFact] = useState();
+  const [catImageUrl, setCatImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(res => setFact(res.fact));
+      .then(res => {
+        const { fact } = res;
+        setFact(fact);
+
+        const threeFirstWords = fact.split(' ', 3).join(' ');
+
+        fetch(catEndpointRandomPhoto(threeFirstWords))
+          .then(res => {
+            setCatImageUrl(res.url);
+          });
+      });
   }, []);
 
   return (
@@ -19,6 +32,13 @@ export default function App () {
 
       <main>
         {fact && <p>{fact}</p>}
+        {
+        catImageUrl &&
+          <img
+            src={catImageUrl}
+            alt={'Image extracted using the first three words of ' + fact}
+          />
+        }
       </main>
     </>
   );
