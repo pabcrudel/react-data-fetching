5cf5c1da4ea780bb68d4aa04bdd948fce669e72d
pabcrudel <pabcrudel@gmail.com>
2023-12-17 20:50:48 +0100

    change: Move App outside entry point

--- assets/main.jsx
+++
@@ -1,4 +0,0 @@
-import './style.css';
-import { createRoot } from 'react-dom/client';
-
-createRoot(document.getElementById('app')).render(<h1>Hello world</h1>);
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App';
+
+createRoot(document.getElementById('app')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
